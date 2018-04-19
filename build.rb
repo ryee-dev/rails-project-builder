8bd77931a8f5c819164795e1b5955cccccfb43ac
roger yee <ryee.epicodus@gmail.com>
2018-04-18 20:26:08 -0700

    adds gems,  shoulda matcher, and creates table

--- build.rb
+++ build.rb
@@ -2,8 +2,9 @@
 require 'fileutils'
 
 class Project
-  def initialize(project_name)
+  def initialize(project_name, table_name)
     @project_name = project_name
+    @table_name = table_name
   end
 
   def build
@@ -11,9 +12,12 @@ class Project
     FileUtils.cd '..'
     puts FileUtils.pwd
 
-    FileUtils.cd("#{@project_name}")
+    FileUtils.cd "#{@project_name}"
 
-    File.open('Gemfile', 'a') do |f|
+    # Adds necessary gems to Gemfile
+    File.open('Gemfile', 'a+') do |f|
+      f << "\n"
+      f << "# gems added through rails-project-builder\n"
       f << "gem 'jquery-rails'\n"
       f << "gem 'bootstrap', '~> 4.0.0.beta2.1'\n"
       f << "gem 'rspec-rails'\n"
@@ -22,15 +26,38 @@ class Project
       f << "gem 'shoulda-matchers'\n"
       f << "gem 'capybara'\n"
     end
+
+    # Creates spec folder with rails_helper and spec_helper
+    system "bundle exec rails generate rspec:install"
+
+    # Adds Shoulda Matcher functionality
+    File.open('spec/rails_helper.rb', 'a+') do |f|
+      f << "Shoulda::Matchers.configure do |config|\n"
+      f << "  config.integrate do |with|\n"
+      f << "    with.test_framework :rspec\n"
+      f << "    with.library :rails\n"
+      f << "  end\n"
+      f << "end\n"
+    end
+
+    # Creates Database
+    system "rails db:create"
+
+    # Creates migration file
+    system "rails g migration create #{@table_name}"
+
+    system "rails db:migrate"
+
   end
 end
 
-
 puts "Enter Project Name: "
 project_name = gets.chomp
+puts "Enter Table Name (must be plural):  "
+table_name = gets.chomp
 
 # eventually allows the user to generate tables
 # puts "Enter Table Name:"
 # puts "Enter Column Names and Datatypes"
-project = Project.new(project_name)
+project = Project.new(project_name, table_name)
 project.build
